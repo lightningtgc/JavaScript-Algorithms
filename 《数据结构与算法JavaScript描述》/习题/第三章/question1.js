ac7362138f626754d5174ccc4507b2ea6a86ea9c
lightningtgc <lightningtgc@gmail.com>
2014-10-09 00:20:59 +0800

    answer1

--- 《数据结构与算法JavaScript描述》/习题/第三章/question1.js
+++ 《数据结构与算法JavaScript描述》/习题/第三章/question1.js
@@ -7,3 +7,48 @@
  *
  */
 
+//评注：该题出得有点语义不明，
+//对于数字字母混合的情况没有说明
+//故暂以都是字母或者数字为例
+
+
+/*
+* tools
+* a与b的大小比较，
+* 若a大于b则返回true
+*/
+function compareSequence(a, b){
+    
+    //如果是字符串的数字( "11" < "2" )的处理
+    var num1 = parseInt(a, 10),
+        num2 = parseInt(b, 10);
+    if (typeof(num1) === "number" && typeof(num2) === "number") {
+        if (num1 > num2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //字母的处理
+    if (a.toLowerCase() > b.toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+//注这里需配合课本中实现的List列表类来解答
+function insertBiggest (element) {
+    for (var i=0; i < this.dataStore.length; i++) {
+
+        //需大于列表全部元素
+        if ( !compareSequence(element, this.dataStore[i]) ) {
+            return false;
+        }
+    }
+
+    this.append(element);
+    return true;
+}
