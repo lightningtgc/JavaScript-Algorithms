b94595844c5f93b2180d0903ac77e93b2d2c59d7
gctang <lightningtgc@gmail.com>
2015-06-07 01:58:36 +0800

    cpp version 8ms

--- leetcode/isomorphic-strings.js
+++ leetcode/isomorphic-strings.js
@@ -18,7 +18,7 @@
 * You may assume both s and t have the same length.
 */
 
-
+// 耗时：128ms
 /**
  * @param {string} s
  * @param {string} t
@@ -43,4 +43,41 @@ var isIsomorphic = function(s, t) {
    }
     
     return true;
-}
+};
+
+// C++最优版本 耗时： 8ms
+class Solution {  
+public:  
+    bool isIsomorphic(string s, string t) {  
+        int sTotTable[256];  
+        int tTosTable[256];  
+        int size = s.length();  
+        for (int i = 0; i < 256; i++)  
+        {  
+            sTotTable[i] = 300;  
+            tTosTable[i] = 300;  
+        }  
+  
+        for (int i = 0; i < size; i++)  
+        {  
+            if (sTotTable[s[i]] == 300)  
+            {  
+                if (tTosTable[t[i]] == 300)  
+                {  
+                    sTotTable[s[i]] = t[i];  
+                    tTosTable[t[i]] = s[i];  
+                }  
+                else  
+                {  
+                    return false;  
+                }  
+            }  
+            else if (sTotTable[s[i]] != t[i])  
+            {  
+                return false;  
+            }  
+        }  
+  
+        return true;  
+    }  
+};  
