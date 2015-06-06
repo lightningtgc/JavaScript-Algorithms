cd282774ac9e9122c4213963323ccb82d8105f3f
gctang <lightningtgc@gmail.com>
2015-06-06 12:23:46 +0800

    comment

--- leetcode/contain-duplicate.js
+++ leetcode/contain-duplicate.js
@@ -4,7 +4,7 @@
 * Your function should return true if any value appears at least twice in the array, 
 * and it should return false if every element is distinct.
 *  
-* 
+* 注释： 判断一个数组是否有重复元素，如果重复则返回true,反之返回false
 *
 * Runtime: 136 ms
 */
@@ -18,7 +18,7 @@ var containsDuplicate = function(nums) {
     var tempHash = {};
     var i = nums.length -1;
     var isDistinct = false;
-    
+    //注意空数组情况，倒序提高速度
     while (0 <= i){
         if (!tempHash[nums[i]]){
             tempHash[nums[i]] = true;
