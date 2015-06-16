a29d8befee6f2957d860407de5325bf459c63ddb
gctang <lightningtgc@gmail.com>
2015-06-16 23:39:13 +0800

    Update house-robber.js

--- leetcode/house-robber.js
+++ leetcode/house-robber.js
@@ -21,15 +21,15 @@
 var rob = function(nums) {
      var currMaxPV = 0, //当前最大值：上一轮循环中的最大值加上当前值
         lastRealMax = 0, //上一轮循环中的真正的最大值
-        i = nums.length -1, 
-        tempLastMaxPV, //
-        realLastMax;
+        i = nums.length -1;
+        
         while ( 0 <= i) { // eg: [2,1,1,2]
-            tempLastMaxPV = currMaxPV; //2 0 3
-            realLastMax = lastRealMax; //0 2 2
-            currMaxPV = realLastMax + nums[i];//1 3 4
-            lastRealMax = tempLastMaxPV > realLastMax ? tempLastMaxPV :realLastMax;//2 2 3
-            i--;
+          var tempLastMaxPV = currMaxPV, //2 0 3
+              realLastMax = lastRealMax; //0 2 2
+          currMaxPV = realLastMax + nums[i];//1 3 4
+          lastRealMax = Math.max(tempLastMaxPV, realLastMax);//2 2 3
+          i--;
         }
-        return currMaxPV > lastRealMax ? currMaxPV : lastRealMax;
+        return Math.max(currMaxPV, lastRealMax); // 4
 };
+
