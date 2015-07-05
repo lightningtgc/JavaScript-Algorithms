2a0c53de36ca0e7e04d5515d04206562d0e960ba
gctang <lightningtgc@gmail.com>
2015-07-05 23:25:42 +0800

    洗牌原理

--- 经典算法/shuffle.js
+++ 经典算法/shuffle.js
@@ -1,4 +1,12 @@
+/**
+ *  洗牌算法：
+ *  1. 从第一张牌开始，将每张牌和随机的一张牌进行交换
+ *  2. 更优解法：从第一张牌开始，将每张牌和之前全部牌中随机的一张进行交换
+ *  3. C++ 标准库函数 std::random_shuffle (原理与第一点差不多)
+ * /
+
 //inspired by http://www.cs.princeton.edu/~rs/
+
 function shuffle(arr) {
   var len = arr.length, i;
   while (len) {
@@ -7,3 +15,4 @@ function shuffle(arr) {
   }
   return arr;
 }
+
