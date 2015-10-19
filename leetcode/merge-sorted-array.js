c53a90d5f9b499124d3f53e92151f43575cdbdca
gctang <lightningtgc@gmail.com>
2015-10-19 15:06:35 +0800

    Update merge-sorted-array.js

--- leetcode/merge-sorted-array.js
+++ leetcode/merge-sorted-array.js
@@ -1,7 +1,11 @@
+/**
+ * 地址：https://leetcode.com/problems/merge-sorted-array/
 
-// 地址：https://leetcode.com/problems/merge-sorted-array/
+ * Question：Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
 
-// 题目：Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
-
-// Note:
-// You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2. The number of elements initialized in nums1 and nums2 are m and n respectively.
+ * Note:
+ * You may assume that nums1 has enough space (size that is greater or equal to m + n) 
+ * to hold additional elements from nums2. 
+ * The number of elements initialized in nums1 and nums2 are m and n respectively.
+ * 
+ * /
