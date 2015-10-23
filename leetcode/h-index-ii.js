6fc0de623692aef69a235487915d83605223afde
gctang <lightningtgc@gmail.com>
2015-10-22 17:10:59 -0900

    Update h-index-ii.js

--- leetcode/h-index-ii.js
+++ leetcode/h-index-ii.js
@@ -1,4 +1,6 @@
 /*
-* Follow up for H-Index(https://leetcode.com/problems/h-index/): What if the citations array is sorted in ascending order? Could you optimize your algorithm?
+* Follow up for H-Index(https://leetcode.com/problems/h-index/):
+* What if the citations array is sorted in ascending order? 
+* Could you optimize your algorithm?
 
 */
