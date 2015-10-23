4548443c12c124eb72efac349b8a2aa562b4fbf0
gctang <lightningtgc@gmail.com>
2015-10-22 17:11:15 -0900

    Update h-index-ii.js

--- leetcode/h-index-ii.js
+++ leetcode/h-index-ii.js
@@ -1,6 +1,6 @@
 /*
 * Follow up for H-Index(https://leetcode.com/problems/h-index/):
-* What if the citations array is sorted in ascending order? 
-* Could you optimize your algorithm?
-
+*   What if the citations array is sorted in ascending order? 
+*   Could you optimize your algorithm?
+*
 */
